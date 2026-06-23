f718eee3994418b0c39216e65d44d8582454cf9f
asingizwe1 <asingizwe1louis1@gmail.com>
2026-06-23 23:29:39 +0300

    full set up to airdrop bagel tokens

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -7,8 +7,13 @@ import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
     contract MerkleAirdropTest is Test {
 MerkleAirdrop private airdrop;
 BagelToken private bagel;
+//creatd coz we dont want magic numbers in function calls
+uint256 public AMOUNT_TO_SEND=AMOUNT_TO_CLAIM*4;
 bytes32 public ROOT="0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4";
-uint256 public AMOUNT = 25 * 1e18;
+uint256 public AMOUNT_TO_CLAIM = 25 * 1e18;
+//the values in the array must first be stored as intermediate bytes32
+bytes32 proofOne=;
+bytes32 proofTwo=;
 bytes32[] public PROOF = [];//copy proofs into the test file
 address public user = "0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D";
 uint256 userPrivkey;
@@ -16,6 +21,9 @@ uint256 userPrivkey;
 function setUp() public {
     bagel = new BagelToken();
     airdrop = new MerkleAirdrop(ROOT,token);
+    token.mint(token.owner(),AMOUNT_TO_SEND);
+    token.transfer(address(airdrop),AMOUNT_TO_SEND);//airdrop comtains all tokens for the airdrop
+    //since airdrop is of type mERKLEROOT We must cast the airdrop above to address
     (user,userPrivkey) = makeAddrAndKey("user");//craete an address and private key
 }
 
@@ -23,12 +31,12 @@ function testUsersCanClaim() public{
 console.log("User address: %s", user);//you opy the address you get and put it in array of addresses
 //we first store their initial address
 uint256 startignBalance = token.balanceOf(user);
-airdrop.claim(user,AMOUNT,PROOF);
+airdrop.claim(user,AMOUNT_TO_CLAIM,PROOF);
 
 vm.prank(user);//prank pranks the next line
 //we use the prank function to simulate the user calling the claim function
 uint256 endingBalance = token.balanceOf(user);
 console.log("User balance after claim: %s", endingBalance);
-assertEq(endingBalance, startignBalance + AMOUNT, "User did not receive the correct amount of tokens");//to check that amount has been sent to the user
+assertEq(endingBalance, startignBalance + AMOUNT_TO_CLAIM   , "User did not receive the correct amount of tokens");//to check that amount has been sent to the user
 }
     }//6:02
